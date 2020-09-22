6ba5762d91ae403edb0a9956baa61f08a0075fcc
felipe costa <felipe.costa@mambo.com.br>
2020-09-22 16:44:41 -0300

    start lamborghini clone template

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Header from '../components/Header/Header'
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-black ">
+      <div>
+        <Header />
+      </div>
+      <main>
+        {/* <Hero />
+        <Models />
+        <News />
+        <Footer /> */}
+      </main>
+    </div>
+  );
+}
