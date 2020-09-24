8ddf30ec7e3da547096911f68946eb202a10e4be
felipe costa <felipe.costa@mambo.com.br>
2020-09-24 07:14:00 -0300

    pull of master

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -13,67 +13,93 @@ export default function News() {
         </div>
 
         <img
-          src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/news/2020/09_21_esport_final/esport_final_cover.jpg"
+          src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/news/2020/09_23_unica/unica_cover1.jpg"
           alt="Lamborghini World"
         />
       </div>
-      <div className="pt-12 pb-16">
+      <div className="pt-16 pb-16">
         <div>
           <h2 className="text-xl uppercase font-medium pb-4">
-            21 september 2020
+            23 september 2020
           </h2>
-          <h3 className="text-lg uppercase px-4  bg-gray-300 w-1/12 hover:bg-black hover:text-white">
-            Esports
-          </h3>
         </div>
         <div>
           <div className="flex items-center justify-between">
-            <h1 className="text-4xl font-semibold uppercase leading-none w-9/12 tracking-wider pt-4">
-              Join us for the thrilling final of lamborghini's the real race
+            <h1 className="text-4xl font-bold uppercase leading-none w-9/12 tracking-wider pt-4">
+              Unica, the app tha connects you to the world of lamborghini
             </h1>
-            <button className="border border-gray-500 uppercase py-4 px-16 hover:bg-black hover:text-white">
+            <button className="border border-gray-500 uppercase py-4 px-20 hover:bg-black hover:text-white text-lg">
               Read more
             </button>
           </div>
           <p className="py-4 text-xl font-light">
-            The Real Race, Lamborghini’s first ever global eSports competition
-            has reached its electrifying finale.
+            With the princeless Unica app, reserved exclusively for Lamborghini
+            owners, you can enjoy a world of <br /> exciting features just by
+            tapping your finger.
           </p>
         </div>
       </div>
-      <div className="flex h-full">
-        <div className="w-1/2">
-          <img
-            src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/news/2020/09_16_ravensburger_evo/hura_evo_raven_cover.jpg"
-            alt="Huracan"
-          />
+      <div className='w-full'>
+        <div className="flex h-full w-full">
+          <div className="w-1/2">
+            <img
+              src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/news/2020/09_21_esport_final/esport_final_cover.jpg"
+              alt="Huracan"
+            />
+          </div>
+          <div className="flex flex-col items-start max-h-full px-12 ">
+            <div>
+              <h2 className="uppercase text-lg font-normal">
+                21 september 2020
+              </h2>
+            </div>
+            <div className="flex flex-col h-full items-start justify-center">
+              <h3 className="px-3  bg-gray-200 hover:bg-black hover:text-white uppercase text-base">
+                Esports
+              </h3>
+              <h1 className="py-1 text-3xl font-bold  w-full text-justify uppercase">
+                Join us for the thrilling <br />
+                final of lamborghini's the real race
+              </h1>
+            </div>
+            <div className="flex flex-col  h-full items-start justify-end w-1/2">
+              <button className="border border-gray-400 border-solid px-16 py-4 uppercase font-normal hover:bg-black hover:text-white">
+                Read More
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col items-start max-h-full px-12 ">
-          <div>
-            <h2 className="uppercase text-lg font-normal">16 september 2020</h2>
+        <div className="flex w-full py-16">
+          <div className="w-1/2">
+            <img
+              src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/news/2020/09_16_ravensburger_evo/hura_evo_raven_cover.jpg"
+              alt="motorsport"
+            />
           </div>
-          <div className="flex flex-col h-full items-start justify-center py-8">
-            <h3 className="px-3  bg-gray-200 hover:bg-black hover:text-white uppercase text-base">
-              Huracán
-            </h3>
-            <h1 className="py-3 text-3xl font-bold  w-full text-justify uppercase">
-              Buid your own lamborghini <br />
-              huracán evo as a 3d puzzle
-            </h1>
-          </div>
-          <div className="flex flex-col  h-full items-start justify-end w-1/2">
-            <button className="border border-gray-400 border-solid px-16 py-4 uppercase font-normal hover:bg-black hover:text-white">
-              Read More
-            </button>
+          <div className="flex flex-col items-start max-h-full px-12 ">
+            <div>
+              <h2 className="uppercase text-lg font-normal">
+                16 september 2020
+              </h2>
+            </div>
+            <div className="flex flex-col h-full items-start justify-center py-8">
+              <h3 className="px-3  bg-gray-200 hover:bg-black hover:text-white uppercase text-base">
+                Huracán
+              </h3>
+              <h1 className="py-3 text-3xl font-bold  w-full text-justify uppercase">
+                Buid your own lamborghini <br />
+                huracán evo as a 3d puzzle
+              </h1>
+            </div>
+            <div className="flex flex-col  h-full items-start justify-end w-1/2">
+              <button className="border border-gray-400 border-solid px-16 py-4 uppercase font-normal hover:bg-black hover:text-white">
+                Read More
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='flex w-full py-16'>
-        <div className='w-1/2'>
-          <img
-            src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/news/2020/motorsport/15_06_essenza/cover.jpg"
-            alt="motorsport"
-          />
+        <div className='flex w-full justify-center items-center py-12'>
+          <button className='border border-solid border-black bg-black w-1/6 py-4 uppercase font-semibold text-white hover:bg-white hover:text-black'>see all</button>
         </div>
       </div>
     </div>
